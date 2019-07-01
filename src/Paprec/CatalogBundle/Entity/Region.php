5b8922f4e07eab9d79a77209e4c426684a6786ef
Frederic Laine <frederic.laine@eggers-digital.com>
2019-07-01 17:10:52 +0200

    [ADD] gestion des régions et codes postaux

---
+++ src/Paprec/CatalogBundle/Entity/Region.php
@@ -0,0 +1,279 @@
+<?php
+
+namespace Paprec\CatalogBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * Region
+ *
+ * @ORM\Table(name="region")
+ * @ORM\Entity(repositoryClass="Paprec\CatalogBundle\Repository\RegionRepository")
+ */
+class Region
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateCreation", type="datetime")
+     */
+    private $dateCreation;
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="dateUpdate", type="datetime", nullable=true)
+     */
+    private $dateUpdate;
+
+
+    /**
+     * @var \DateTime
+     *
+     * @ORM\Column(name="deleted", type="datetime", nullable=true)
+     */
+    private $deleted;
+
+    /**
+     * #################################
+     *              SYSTEM USER ASSOCIATION
+     * #################################
+     */
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Paprec\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="userCreationId", referencedColumnName="id", nullable=false)
+     */
+    private $userCreation;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="Paprec\UserBundle\Entity\User")
+     * @ORM\JoinColumn(name="userUpdateId", referencedColumnName="id", nullable=true)
+     */
+    private $userUpdate;
+
+    /**
+     * @var string
+     *
+     * @ORM\Column(name="name", type="string", length=255)
+     */
+    private $name;
+
+    /**
+     * #################################
+     *              Relations
+     * #################################
+     */
+
+    /**
+     * @ORM\OneToMany(targetEntity="Paprec\CatalogBundle\Entity\PostalCode", mappedBy="region")
+     */
+    private $postalCodes;
+
+
+    /**
+     * Get id.
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set name.
+     *
+     * @param string $name
+     *
+     * @return Region
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name.
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    /**
+     * Set dateCreation.
+     *
+     * @param \DateTime $dateCreation
+     *
+     * @return Region
+     */
+    public function setDateCreation($dateCreation)
+    {
+        $this->dateCreation = $dateCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get dateCreation.
+     *
+     * @return \DateTime
+     */
+    public function getDateCreation()
+    {
+        return $this->dateCreation;
+    }
+
+    /**
+     * Set dateUpdate.
+     *
+     * @param \DateTime|null $dateUpdate
+     *
+     * @return Region
+     */
+    public function setDateUpdate($dateUpdate = null)
+    {
+        $this->dateUpdate = $dateUpdate;
+
+        return $this;
+    }
+
+    /**
+     * Get dateUpdate.
+     *
+     * @return \DateTime|null
+     */
+    public function getDateUpdate()
+    {
+        return $this->dateUpdate;
+    }
+
+    /**
+     * Set userCreation.
+     *
+     * @param \Paprec\UserBundle\Entity\User $userCreation
+     *
+     * @return Region
+     */
+    public function setUserCreation(\Paprec\UserBundle\Entity\User $userCreation)
+    {
+        $this->userCreation = $userCreation;
+
+        return $this;
+    }
+
+    /**
+     * Get userCreation.
+     *
+     * @return \Paprec\UserBundle\Entity\User
+     */
+    public function getUserCreation()
+    {
+        return $this->userCreation;
+    }
+
+    /**
+     * Set userUpdate.
+     *
+     * @param \Paprec\UserBundle\Entity\User|null $userUpdate
+     *
+     * @return Region
+     */
+    public function setUserUpdate(\Paprec\UserBundle\Entity\User $userUpdate = null)
+    {
+        $this->userUpdate = $userUpdate;
+
+        return $this;
+    }
+
+    /**
+     * Get userUpdate.
+     *
+     * @return \Paprec\UserBundle\Entity\User|null
+     */
+    public function getUserUpdate()
+    {
+        return $this->userUpdate;
+    }
+    /**
+     * Constructor
+     */
+    public function __construct()
+    {
+        $this->postalCodes = new \Doctrine\Common\Collections\ArrayCollection();
+    }
+
+    /**
+     * Add postalCode.
+     *
+     * @param \Paprec\CatalogBundle\Entity\PostalCode $postalCode
+     *
+     * @return Region
+     */
+    public function addPostalCode(\Paprec\CatalogBundle\Entity\PostalCode $postalCode)
+    {
+        $this->postalCodes[] = $postalCode;
+
+        return $this;
+    }
+
+    /**
+     * Remove postalCode.
+     *
+     * @param \Paprec\CatalogBundle\Entity\PostalCode $postalCode
+     *
+     * @return boolean TRUE if this collection contained the specified element, FALSE otherwise.
+     */
+    public function removePostalCode(\Paprec\CatalogBundle\Entity\PostalCode $postalCode)
+    {
+        return $this->postalCodes->removeElement($postalCode);
+    }
+
+    /**
+     * Get postalCodes.
+     *
+     * @return \Doctrine\Common\Collections\Collection
+     */
+    public function getPostalCodes()
+    {
+        return $this->postalCodes;
+    }
+
+    /**
+     * Set deleted.
+     *
+     * @param \DateTime|null $deleted
+     *
+     * @return Region
+     */
+    public function setDeleted($deleted = null)
+    {
+        $this->deleted = $deleted;
+
+        return $this;
+    }
+
+    /**
+     * Get deleted.
+     *
+     * @return \DateTime|null
+     */
+    public function getDeleted()
+    {
+        return $this->deleted;
+    }
+}
